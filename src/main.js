7962c62ce61684c6185e1de49c5b317d9f678842
Artfa <artur7almeida@gmail.com>
2020-04-23 18:15:22 -0300

    first

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'vue-tableau'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
